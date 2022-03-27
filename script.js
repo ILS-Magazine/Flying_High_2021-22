71b37818c1f3a43ff320f8cf5a9156bafe6a0bdb
Gaurav Shah <gauravshahstudies@gmail.com>
2022-03-27 19:45:04 +0100

    Updating Code

---
+++ script.js
@@ -0,0 +1,30 @@
+var darkModeNumber = localStorage.getItem("darkModeNumber");
+
+if (darkModeNumber == 1) {
+    darkMode();
+}
+
+//Chech if Dark mode is already enabled
+
+function checkDarkMode() {
+    if (localStorage.getItem("darkModeNumber") == 1) {
+        localStorage.setItem("darkModeNumber", 0);
+        lightMode();
+    } else {
+        localStorage.setItem("darkModeNumber", 1);
+        darkMode();
+    }
+}
+
+function darkMode() {
+    var root = document.querySelector(':root');
+    root.style.setProperty('--light', '#020f17');
+    root.style.setProperty('--dark', '#f8efe6');
+
+}
+
+function lightMode() {
+    var root = document.querySelector(':root');
+    root.style.setProperty('--dark', '#020f17');
+    root.style.setProperty('--light', '#f8efe6');
+}
