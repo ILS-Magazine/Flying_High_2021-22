5794974e5c07c85b98176ca107f0f7f7285fb3aa
Gaurav Shah <gauravshahstudies@gmail.com>
2022-03-30 10:42:43 +0100

    Dark Mode Button

---
+++ Experiments/Gaurav/Dark Mode Buttons/gaurav-dark-mode.js
@@ -0,0 +1,32 @@
+var darkModeNumber = localStorage.getItem("darkModeNumber");
+var darkModeButton = document.querySelector(".darkModeButton");
+
+if (darkModeNumber == 1) {
+    darkMode();
+}
+
+//Chech if Dark mode is already enabled
+
+function checkDarkMode() {
+    if (localStorage.getItem("darkModeNumber") == 1) {
+        localStorage.setItem("darkModeNumber", 0);
+        lightMode();
+    } else {
+        localStorage.setItem("darkModeNumber", 1);
+        darkMode();
+    }
+}
+
+function darkMode() {
+    var root = document.querySelector(':root');
+    root.style.setProperty('--light', '#020f17');
+    root.style.setProperty('--dark', '#f8efe6');
+    darkModeButton.style.backgroundImage = "url(sun.svg)";
+}
+
+function lightMode() {
+    var root = document.querySelector(':root');
+    root.style.setProperty('--dark', '#020f17');
+    root.style.setProperty('--light', '#f8efe6');
+    darkModeButton.style.backgroundImage = "url(moon.svg)";
+}
