a4f50e5ed51f102cab0ffdaadc7a60dccc62efd9
Gaurav Shah <gauravshahstudies@gmail.com>
2022-04-19 23:16:50 +0100

    iug

---
+++ Experiments/Gaurav/Gallery Cards/gallery-cards.js
@@ -0,0 +1,7 @@
+`<div class="card">
+    <div class="cardImage">
+        <img src="https://images.unsplash.com/photo-1525183995014-bd94c0750cd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80"
+            alt="Beach">
+    </div>
+    <p class="cardName">`+studentName+`</p>
+</div>`
