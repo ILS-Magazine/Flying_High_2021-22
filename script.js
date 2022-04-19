b4df99c75495f94133c623fe4abfa96c9f02ef84
Gaurav Shah <gauravshahstudies@gmail.com>
2022-04-19 22:35:10 +0100

    NavBar

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var darkModeNumber = localStorage.getItem("darkModeNumber");
-var darkModeIcon = document.getElementById("darkModeIcon");
+var darkModeButton = document.querySelector(".darkModeButton");
 
 if (darkModeNumber == 1) {
     darkMode();
 }
 
-//Chech if Dark mode is already enabled
+//Check if Dark mode is already enabled
 
 function checkDarkMode() {
     if (localStorage.getItem("darkModeNumber") == 1) {
@@ -21,13 +21,12 @@ function darkMode() {
     var root = document.querySelector(':root');
     root.style.setProperty('--light', '#020f17');
     root.style.setProperty('--dark', '#f8efe6');
-    darkModeIcon.src = "./Images/Dark Mode Buttons/LightbulbOn.svg";
-
+    darkModeButton.style.backgroundImage = "url(./Images/NavBar/sun.svg)";
 }
 
 function lightMode() {
     var root = document.querySelector(':root');
     root.style.setProperty('--dark', '#020f17');
     root.style.setProperty('--light', '#f8efe6');
-    darkModeIcon.src = "./Images/Dark Mode Buttons/LightbulbOff.svg";
+    darkModeButton.style.backgroundImage = "url(./Images/NavBar/moon.svg)";
 }
