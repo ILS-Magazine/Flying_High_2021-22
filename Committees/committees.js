183d4c11d67f4c2b6524163b6aed3c57e8cbf42f
Gaurav Shah <gauravshahstudies@gmail.com>
2022-07-04 22:45:54 +0100

    Com

--- Committees/committees.js
+++ Committees/committees.js
@@ -1,202 +1,230 @@
 Committees = [
-    {
-      "Committee": "Student Voice Group",
-      "Short Form": "SVG",
-      "Teacher Head": "Mrs. S. Gupta",
-      "Student Head 1": "Shreyas Rajesh ",
-      "Student Head 2": "Keyush Venket Subramanian",
-      "Description": "is the Voice of the students of ILS. The group endeavours to find solutions to the problems faced by the students."
-    },
-    {
-      "Committee": "Hubs Of Learning",
-      "Short Form": "HOLC",
-      "Teacher Head": "Mrs. S. Gupta and Dr. (Mrs.) M. Jain",
-      "Student Head 1": "Aryan Babaria",
-      "Student Head 2": "",
-      "Description": "are formed for collaborative growth, promoting self - improvement and for ensuring the enhancement of the quality of education."
-    },
-    {
-      "Committee": "Eco Club",
-      "Short Form": "ECOC",
-      "Teacher Head": "Dr. (Mrs.) M. Jain ",
-      "Student Head 1": "Akhilesh Ghate",
-      "Student Head 2": "",
-      "Description": "empowers students to take up meaningful environmental activities and projects. It is a forum through which students can reach out to influence, engage their parents and neighbourhood communities to promote sound environmental behaviour. "
-    },
-    {
-      "Committee": "School Safety Committee",
-      "Short Form": "SSC",
-      "Teacher Head": "Mrs. I. Yadav and Mrs. N. Goyal",
-      "Student Head 1": "Vedant Vadalikar",
-      "Student Head 2": "",
-      "Description": "is meant to make the students aware of the basic Safety needs. It promotes the protection of students from bullying, injuries, illness, harassment and other safety issues."
-    },
-    {
-      "Committee": "Events Committee",
-      "Short Form": "EC",
-      "Teacher Head": "",
-      "Student Head 1": "Krish Bansal",
-      "Student Head 2": "",
-      "Description": "of the school has applied the basic framework in the meticulously conduction of any event in the school. Planning an event goes much more smoothly with a proper team in place. "
-    },
-    {
-      "Committee": "Global Awareness Committee",
-      "Short Form": "GAC",
-      "Teacher Head": "Mrs. B. Mathew ",
-      "Student Head 1": "Sahaj Falod",
-      "Student Head 2": "",
-      "Description": "which consists of Senior Students of ILS has undertaken an initiative to keep the students updated of Global Events"
-    },
-    {
-      "Committee": "Curriculum Development Group",
-      "Short Form": "CDG",
-      "Teacher Head": "Mrs. R. Lakhanpal ",
-      "Student Head 1": "Raghav Kumar Prasad ",
-      "Student Head 2": "Sanjith Santhosh",
-      "Description": "consist of Senior Students. Its implementation focusses on development of critical thinking, problem solving skills across the school."
-    },
-    {
-      "Committee": "Student Counselling Committee",
-      "Short Form": "SCC",
-      "Teacher Head": "Mrs. G. Bajaj ",
-      "Student Head 1": "Sarah Santhosh Abraham",
-      "Student Head 2": "",
-      "Description": "aims to ensure equitable academic, career and social/emotional development opportunities for all students."
-    },
-    {
-      "Committee": "Magazine Committee",
-      "Short Form": "MC",
-      "Teacher Head": "Mrs. G. Shergill",
-      "Student Head 1": "Subhashree Panigrahy ",
-      "Student Head 2": "Harshala Kumar",
-      "Description": "works throughout the year to collect best creative writings, record prestigious awards, memories in form of pictures and delivers a school magazine annually."
-    },
-    {
-      "Committee": "Art Integration Committee",
-      "Short Form": "AIC",
-      "Teacher Head": "Mrs. N. Ramchandani ",
-      "Student Head 1": "Gaurav Shah ",
-      "Student Head 2": "Ananya Jayaprakash Rao",
-      "Description": "is an approach to teaching that integratesthefine and performingartsas primary pathways to learning."
-    },
-    {
-      "Committee": "Career Counselling Committee",
-      "Short Form": "CCC",
-      "Teacher Head": "Mrs. S. Tandon ",
-      "Student Head 1": "Lakshita Aggarwal ",
-      "Student Head 2": "Khushee Joshi",
-      "Description": "help students develop skills, explore a career, or choose an educational program that will lead to a career."
-    },
-    {
-      "Committee": "Interact Club",
-      "Short Form": "IC",
-      "Teacher Head": "Mrs. A. Dhawan ",
-      "Student Head 1": "Hemani Rao",
-      "Student Head 2": "",
-      "Description": "members carry out hands-on service projects, make international connections, develop leadership skills, and have fun in the process."
-    },
-    {
-      "Committee": "Sewa",
-      "Short Form": "SEWA",
-      "Teacher Head": "Mrs. L. Chandrashekhar ",
-      "Student Head 1": "Nivedita Shukla",
-      "Student Head 2": "",
-      "Description": "(Social Empowerment through Work Education and Action) an endeavor byCBSEis a program that aims to focus on the mental, emotional and social health of a student."
-    },
-    {
-      "Committee": "Health And Wellness Committee",
-      "Short Form": "HAWC",
-      "Teacher Head": "Mrs. G. Manaise ",
-      "Student Head 1": "Aaisha Dilip Sawlani ",
-      "Student Head 2": "Richa Sali ",
-      "Description": "'s role is to build an environment that is supportive of living a healthy life style."
-    },
-    {
-      "Committee": "Charity Committee",
-      "Short Form": "CC",
-      "Teacher Head": "Mrs. A. Chawla",
-      "Student Head 1": "Rahul Arora ",
-      "Student Head 2": "Varsha Susan Issac",
-      "Description": "helps to raise money for those in need."
-    },
-    {
-      "Committee": "Model United Nations Committee",
-      "Short Form": "MUN",
-      "Teacher Head": "Mrs. L. Dam ",
-      "Student Head 1": "Harsift Kaur ",
-      "Student Head 2": "Vimudha Jinal Singh ",
-      "Description": "help the students to develop leadership skills, research, writing, public speaking, and problem-solving skills."
-    },
-    {
-      "Committee": "Event Logger Committee",
-      "Short Form": "ELC",
-      "Teacher Head": "Mrs. N. Sharma",
-      "Student Head 1": "K.P.Niranjini",
-      "Student Head 2": "",
-      "Description": "keeps a record of all the school events, with description and pictures of the events. This helps in collating data for the school newsletter and magazine."
-    },
-    {
-      "Committee": "D.E.A.R.",
-      "Short Form": "DEAR",
-      "Teacher Head": "Mrs. S. Mitra",
-      "Student Head 1": "Prakhar Borgaonkar ",
-      "Student Head 2": "Singarapu Tanasi",
-      "Description": "stands for \"Drop Everything and Read,\" a celebration ofreadingdesigned to remind students to makereadinga priority activity in their lives."
-    },
-    {
-      "Committee": "Discipline Committee",
-      "Short Form": "DC",
-      "Teacher Head": "Mrs. P. Gopal",
-      "Student Head 1": "Pratham Muthyampeta ",
-      "Student Head 2": "Nandini Sehgal ",
-      "Description": "has a goal of creating a safe, motivating and accepting  environment by supporting the use of positive attitudes and practices. "
-    },
-    {
-      "Committee": "Life Skills Committee",
-      "Short Form": "LSC",
-      "Teacher Head": "Mrs. S. De ",
-      "Student Head 1": "Vinisha Sahoo",
-      "Student Head 2": "",
-      "Description": "The term ‘Life Skills’ is usually used for any of the skills needed to deal well and effectively with the challenges of life. "
-    },
-    {
-      "Committee": "SportsCommittee",
-      "Short Form": "SC",
-      "Teacher Head": "Dr. (Mrs.) P. Nagine",
-      "Student Head 1": "Keerthivasan Anandaraj ",
-      "Student Head 2": "Garistha Sharma",
-      "Description": "helps promotesportsactivities by motivating students and promoting team spirit by making healthy competition."
-    },
-    {
-      "Committee": "Personality Development Committee",
-      "Short Form": "PDC",
-      "Teacher Head": "Mrs. N. Jha ",
-      "Student Head 1": "Megha Bailur",
-      "Student Head 2": "",
-      "Description": "functions to create zeal amongst students towards personality enhancement. It plays a vital role in improving one's communication skills and facing the turbulent world with strides and smile."
-    },
-    {
-      "Committee": "Mental Health&WellbeingCommittee",
-      "Short Form": "MHAWBC",
-      "Teacher Head": "Dr. (Mrs.) S. Jamloki",
-      "Student Head 1": "Kritika Kaurani ",
-      "Student Head 2": "Anushri Udasi",
-      "Description": "exists to monitor and address the mental health and wellbeing needs of students."
-    },
-    {
-      "Committee": "Culinary Arts Committee",
-      "Short Form": "CAC",
-      "Teacher Head": "Mrs. A. Kaurani ",
-      "Student Head 1": "Noopur Sanjata Roul ",
-      "Student Head 2": "Tanishka Shivnani",
-      "Description": ""
-    },
-    {
-      "Committee": "First Aid Committee",
-      "Short Form": "FAC",
-      "Teacher Head": "Coach Mooney ",
-      "Student Head 1": "Riya Trivedi",
-      "Student Head 2": "",
-      "Description": "aims to provide immediate, lifesaving, medical care before the arrival of further medical help."
-    }
-   ]
+  {
+    "Committee": "Student Voice Group",
+    "ShortForm": "SVG",
+    "TeacherHead": "Mrs. S. Gupta",
+    "StudentHead1": "Shreyas Rajesh ",
+    "StudentHead2": "Keyush Venket Subramanian",
+    "Description": "is the Voice of the students of ILS. The group endeavours to find solutions to the problems faced by the students."
+  },
+  {
+    "Committee": "Hubs Of Learning",
+    "ShortForm": "HOLC",
+    "TeacherHead": "Mrs. S. Gupta and Dr. (Mrs.) M. Jain",
+    "StudentHead1": "Aryan Babaria",
+    "StudentHead2": "",
+    "Description": "are formed for collaborative growth, promoting self - improvement and for ensuring the enhancement of the quality of education."
+  },
+  {
+    "Committee": "Eco Club",
+    "ShortForm": "ECOC",
+    "TeacherHead": "Dr. (Mrs.) M. Jain ",
+    "StudentHead1": "Akhilesh Ghate",
+    "StudentHead2": "",
+    "Description": "empowers students to take up meaningful environmental activities and projects. It is a forum through which students can reach out to influence, engage their parents and neighbourhood communities to promote sound environmental behaviour. "
+  },
+  {
+    "Committee": "School Safety Committee",
+    "ShortForm": "SSC",
+    "TeacherHead": "Mrs. I. Yadav and Mrs. N. Goyal",
+    "StudentHead1": "Vedant Vadalikar",
+    "StudentHead2": "",
+    "Description": "is meant to make the students aware of the basic Safety needs. It promotes the protection of students from bullying, injuries, illness, harassment and other safety issues."
+  },
+  {
+    "Committee": "Events Committee",
+    "ShortForm": "EC",
+    "TeacherHead": "",
+    "StudentHead1": "Krish Bansal",
+    "StudentHead2": "",
+    "Description": "of the school has applied the basic framework in the meticulously conduction of any event in the school. Planning an event goes much more smoothly with a proper team in place. "
+  },
+  {
+    "Committee": "Global Awareness Committee",
+    "ShortForm": "GAC",
+    "TeacherHead": "Mrs. B. Mathew ",
+    "StudentHead1": "Sahaj Falod",
+    "StudentHead2": "",
+    "Description": "which consists of Senior Students of ILS has undertaken an initiative to keep the students updated of Global Events"
+  },
+  {
+    "Committee": "Curriculum Development Group",
+    "ShortForm": "CDG",
+    "TeacherHead": "Mrs. R. Lakhanpal ",
+    "StudentHead1": "Raghav Kumar Prasad ",
+    "StudentHead2": "Sanjith Santhosh",
+    "Description": "consist of Senior Students. Its implementation focusses on development of critical thinking, problem solving skills across the school."
+  },
+  {
+    "Committee": "Student Counselling Committee",
+    "ShortForm": "SCC",
+    "TeacherHead": "Mrs. G. Bajaj ",
+    "StudentHead1": "Sarah Santhosh Abraham",
+    "StudentHead2": "",
+    "Description": "aims to ensure equitable academic, career and social/emotional development opportunities for all students."
+  },
+  {
+    "Committee": "Magazine Committee",
+    "ShortForm": "MC",
+    "TeacherHead": "Mrs. G. Shergill",
+    "StudentHead1": "Subhashree Panigrahy ",
+    "StudentHead2": "Harshala Kumar",
+    "Description": "works throughout the year to collect best creative writings, record prestigious awards, memories in form of pictures and delivers a school magazine annually."
+  },
+  {
+    "Committee": "Art Integration Committee",
+    "ShortForm": "AIC",
+    "TeacherHead": "Mrs. N. Ramchandani ",
+    "StudentHead1": "Gaurav Shah ",
+    "StudentHead2": "Ananya Jayaprakash Rao",
+    "Description": "is an approach to teaching that integratesthefine and performingartsas primary pathways to learning."
+  },
+  {
+    "Committee": "Career Counselling Committee",
+    "ShortForm": "CCC",
+    "TeacherHead": "Mrs. S. Tandon ",
+    "StudentHead1": "Lakshita Aggarwal ",
+    "StudentHead2": "Khushee Joshi",
+    "Description": "help students develop skills, explore a career, or choose an educational program that will lead to a career."
+  },
+  {
+    "Committee": "Interact Club",
+    "ShortForm": "IC",
+    "TeacherHead": "Mrs. A. Dhawan ",
+    "StudentHead1": "Hemani Rao",
+    "StudentHead2": "",
+    "Description": "members carry out hands-on service projects, make international connections, develop leadership skills, and have fun in the process."
+  },
+  {
+    "Committee": "Sewa",
+    "ShortForm": "SEWA",
+    "TeacherHead": "Mrs. L. Chandrashekhar ",
+    "StudentHead1": "Nivedita Shukla",
+    "StudentHead2": "",
+    "Description": "(Social Empowerment through Work Education and Action) an endeavor byCBSEis a program that aims to focus on the mental, emotional and social health of a student."
+  },
+  {
+    "Committee": "Health And Wellness Committee",
+    "ShortForm": "HAWC",
+    "TeacherHead": "Mrs. G. Manaise ",
+    "StudentHead1": "Aaisha Dilip Sawlani ",
+    "StudentHead2": "Richa Sali ",
+    "Description": "'s role is to build an environment that is supportive of living a healthy life style."
+  },
+  {
+    "Committee": "Charity Committee",
+    "ShortForm": "CC",
+    "TeacherHead": "Mrs. A. Chawla",
+    "StudentHead1": "Rahul Arora ",
+    "StudentHead2": "Varsha Susan Issac",
+    "Description": "helps to raise money for those in need."
+  },
+  {
+    "Committee": "Model United Nations Committee",
+    "ShortForm": "MUN",
+    "TeacherHead": "Mrs. L. Dam ",
+    "StudentHead1": "Harsift Kaur ",
+    "StudentHead2": "Vimudha Jinal Singh ",
+    "Description": "help the students to develop leadership skills, research, writing, public speaking, and problem-solving skills."
+  },
+  {
+    "Committee": "Event Logger Committee",
+    "ShortForm": "ELC",
+    "TeacherHead": "Mrs. N. Sharma",
+    "StudentHead1": "K.P.Niranjini",
+    "StudentHead2": "",
+    "Description": "keeps a record of all the school events, with description and pictures of the events. This helps in collating data for the school newsletter and magazine."
+  },
+  {
+    "Committee": "D.E.A.R.",
+    "ShortForm": "DEAR",
+    "TeacherHead": "Mrs. S. Mitra",
+    "StudentHead1": "Prakhar Borgaonkar ",
+    "StudentHead2": "Singarapu Tanasi",
+    "Description": "stands for \"Drop Everything and Read,\" a celebration ofreadingdesigned to remind students to makereadinga priority activity in their lives."
+  },
+  {
+    "Committee": "Discipline Committee",
+    "ShortForm": "DC",
+    "TeacherHead": "Mrs. P. Gopal",
+    "StudentHead1": "Pratham Muthyampeta ",
+    "StudentHead2": "Nandini Sehgal ",
+    "Description": "has a goal of creating a safe, motivating and accepting  environment by supporting the use of positive attitudes and practices. "
+  },
+  {
+    "Committee": "Life Skills Committee",
+    "ShortForm": "LSC",
+    "TeacherHead": "Mrs. S. De ",
+    "StudentHead1": "Vinisha Sahoo",
+    "StudentHead2": "",
+    "Description": "The term ‘Life Skills’ is usually used for any of the skills needed to deal well and effectively with the challenges of life. "
+  },
+  {
+    "Committee": "Sports Committee",
+    "ShortForm": "SC",
+    "TeacherHead": "Dr. (Mrs.) P. Nagine",
+    "StudentHead1": "Keerthivasan Anandaraj ",
+    "StudentHead2": "Garistha Sharma",
+    "Description": "helps promotesportsactivities by motivating students and promoting team spirit by making healthy competition."
+  },
+  {
+    "Committee": "Personality Development Committee",
+    "ShortForm": "PDC",
+    "TeacherHead": "Mrs. N. Jha ",
+    "StudentHead1": "Megha Bailur",
+    "StudentHead2": "",
+    "Description": "functions to create zeal amongst students towards personality enhancement. It plays a vital role in improving one's communication skills and facing the turbulent world with strides and smile."
+  },
+  {
+    "Committee": "Mental Health & Wellbeing Committee",
+    "ShortForm": "MHAWBC",
+    "TeacherHead": "Dr. (Mrs.) S. Jamloki",
+    "StudentHead1": "Kritika Kaurani ",
+    "StudentHead2": "Anushri Udasi",
+    "Description": "exists to monitor and address the mental health and wellbeing needs of students."
+  },
+  {
+    "Committee": "Culinary Arts Committee",
+    "ShortForm": "CAC",
+    "TeacherHead": "Mrs. A. Kaurani ",
+    "StudentHead1": "Noopur Sanjata Roul ",
+    "StudentHead2": "Tanishka Shivnani",
+    "Description": ""
+  },
+  {
+    "Committee": "First Aid Committee",
+    "ShortForm": "FAC",
+    "TeacherHead": "Coach Mooney ",
+    "StudentHead1": "Riya Trivedi",
+    "StudentHead2": "",
+    "Description": "aims to provide immediate, lifesaving, medical care before the arrival of further medical help."
+  }
+]
+
+localStorage.setItem("committee", "AIC");
+
+function cList() {
+  Committees.forEach(committee => {
+    // Getting Details
+    cName = committee.Committee;
+    shortForm = committee.ShortForm;
+    teacherHead = committee.TeacherHead;
+    studentHead1 = committee.StudentHead1;
+    studentHead2 = committee.StudentHead2;
+    description = committee.Description;
+
+    // Creating Committee Div
+    cDiv = `
+    <div class="committee">
+      <img src="./Committee Logos/${shortForm}.svg" alt="${cName}" class="cLogo">
+      <p class="cName">${cName}</p>
+      <p class="cDescription">${description}</p>
+    </div>`
+      ;
+    
+    // Adding to HTML
+    document.querySelector(".pageWrapper").innerHTML += cDiv;
+  })
+}
+
+cList()
