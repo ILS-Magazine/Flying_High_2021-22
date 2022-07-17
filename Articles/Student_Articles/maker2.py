f553f14e0eb5680016fe9bfdd71556aaaac93e64
Dheer Ramnani <ramnanidheer@gmail.com>
2022-07-17 22:36:34 +0100

    awdAWdADawd

--- Articles/Student_Articles/maker2.py
+++ Articles/Student_Articles/maker2.py
@@ -1,1 +1,16 @@
 import pandas as pd
+
+
+df = pd.read_excel('Magazine (1).xlsx')
+name = df["NAME"]
+grade = df["CLASS"]
+imglk = df["IMAGE LINK"]
+topic = df["TOPIC"]
+article = df["ARTICLE"]
+
+
+x = len(name)
+
+for i in range(0, x):
+    articles[i].replace("(", "<p>")
+    articles[i].replace(")", "</p>")
